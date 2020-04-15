16b913b552c4ed982e54a57d0a3ea8bbaa16418b
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-04-15 22:17:38 +0600

    Added three first validators

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,6 +1,24 @@
+import {
+    prop,
+    equals,
+    compose,
+    allPass,
+    filter,
+    gte,
+    values,
+    length,
+    partial,
+    partialRight,
+    countBy,
+    identity,
+    pick,
+} from 'ramda';
+
+import { COLORS, SHAPES } from '../constants';
+
 /**
  * @file Домашка по FP ч. 1
- * 
+ *
  * Основная задача — написать самому, или найти в FP библиотеках функции anyPass/allPass
  * Эти функции/их аналоги есть и в ramda и в lodash
  *
@@ -13,20 +31,55 @@
  * Если какие либо функции написаны руками (без использования библиотек) это не является ошибкой
  */
 
-// 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
-        return false;
-    }
+const equalsRed = equals(COLORS.RED);
+const equalsGreen = equals(COLORS.GREEN);
+const equalsOrange = equals(COLORS.ORANGE);
+const equalsBlue = equals(COLORS.BLUE);
+const equalsWhite = equals(COLORS.WHITE);
 
-    return star === 'red' && square === 'green';
+const getStarColor = prop(SHAPES.STAR);
+const getSquareColor = prop(SHAPES.SQUARE);
+const getTriangleColor = prop(SHAPES.TRIANGLE);
+const getCircleColor = prop(SHAPES.CIRCLE);
+
+// 1. Красная звезда, зеленый квадрат, все остальные белые.
+export const validateFieldN1 = obj => {
+    const isStarColorEqualsRed = compose(equalsRed, getStarColor);
+    const isSquareColorEqualsGreen = compose(equalsGreen, getSquareColor);
+    const isTriangleColorEqualsWhite = compose(equalsWhite, getTriangleColor);
+    const isCircleColorEqualsWhite = compose(equalsWhite, getCircleColor);
+
+    const f = allPass([
+        isStarColorEqualsRed,
+        isSquareColorEqualsGreen,
+        isTriangleColorEqualsWhite,
+        isCircleColorEqualsWhite,
+    ]);
+    return f(obj);
 };
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = obj => {
+    const getGreenShapesObj = partial(filter, [equalsGreen]);
+    const greaterOrEqualThan2 = partialRight(gte, [2]);
+
+    const f = compose(greaterOrEqualThan2, length, values, getGreenShapesObj);
+    return f(obj);
+};
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = obj => {
+    const countByIdentity = countBy(identity);
+    const isEqualNumberOfRedAndBlue = obj => equals(obj[COLORS.RED], obj[COLORS.BLUE]);
+
+    const f = compose(
+        isEqualNumberOfRedAndBlue,
+        pick([COLORS.RED, COLORS.BLUE]),
+        countByIdentity,
+        values
+    );
+    return f(obj);
+};
 
 // 4. Синий круг, красная звезда, оранжевый квадрат
 export const validateFieldN4 = () => false;
